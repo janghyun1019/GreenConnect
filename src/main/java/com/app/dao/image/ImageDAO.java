fff881611cd7ed1f0869bf599ec974311922ef49
PCJ-93 <pw2900@naver.com>
2025-03-05 19:56:54 +0900

    이미지저장 api 작성

---
+++ src/main/java/com/app/dao/image/ImageDAO.java
@@ -0,0 +1,9 @@
+package com.app.dao.image;
+
+import com.app.dto.image.Image;
+
+public interface ImageDAO {
+	
+	int saveImage(Image image);
+
+}
---
+++ src/main/java/com/app/service/image/ImageService.java
@@ -0,0 +1,10 @@
+package com.app.service.image;
+
+import com.app.dto.image.Image;
+
+public interface ImageService {
+	
+	int saveImage(Image image);
+	
+
+}
