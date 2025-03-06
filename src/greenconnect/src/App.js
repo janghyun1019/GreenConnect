1d0dd7e7c8aaee0cfd5f5bd1b175f4bd171b07c9
janghyun1019 <moon.p.a.hyun@gmail.com>
2025-03-06 11:13:54 +0900

    signup 적용

--- src/greenconnect/src/App.js
+++ src/greenconnect/src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import Signup from "./Page/Signup/Signup.js";
 import Login from "./Page/Signup/Login.js";
 import FindId from "./Page/Signup/FindId.js";
@@ -5,31 +6,31 @@ import FindPassword from "./Page/Signup/FindPassword.js";
 import ResetPassword from "./Page/Signup/ResetPassword.js";
 import Header from "./common/Header";
 import Footer from "./common/Footer";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useInRouterContext } from "react-router-dom";
 import Main from "./main/main";
 
-
-
 function App() {
+  // 이미 Router 컨텍스트 내에 있는지 확인합니다.
+  const inRouter = useInRouterContext();
+  const RouterWrapper = inRouter ? React.Fragment : BrowserRouter;
+
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <RouterWrapper basename={!inRouter ? process.env.PUBLIC_URL : undefined}>
       <div className="app-container">
         <Header />
         <Routes>
           <Route path="/" element={<Main />} />
-
         </Routes>
         <Footer />
       </div>
-    </BrowserRouter>
+    </RouterWrapper>
   );
-      // return <Merge/>;
+  // return <Merge/>;
   // return <Signup/>;
-  //return <Login/>;
+  // return <Login/>;
   // return <FindId/>;
-  // return<FindPassword/>;
-  // return<ResetPassword/>;
-
+  // return <FindPassword/>;
+  // return <ResetPassword/>;
 }
 
 export default App;
