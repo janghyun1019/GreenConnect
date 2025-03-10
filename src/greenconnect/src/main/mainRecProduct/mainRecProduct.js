fcf63c36813fd6c64c6ddb41e9b3691b05063316
Jang Hyun <moon.p.a.hyun@gmail.com>
2025-03-11 00:10:27 +0900

    mainRecProduct icon 링크 연결

--- src/greenconnect/src/main/mainRecProduct/mainRecProduct.js
+++ src/greenconnect/src/main/mainRecProduct/mainRecProduct.js
@@ -2,11 +2,11 @@ import React, { useState } from 'react';
 import './mainRecProduct.css';
 
 const categories = [
-  { name: '채소', off: 'icon_off_01.jpg', on: 'icon_on_01.jpg' },
-  { name: '과일', off: 'icon_off_02.jpg', on: 'icon_on_02.jpg' },
-  { name: '나물/버섯', off: 'icon_off_03.jpg', on: 'icon_on_03.jpg' },
-  { name: '꿀/뿌리', off: 'icon_off_04.jpg', on: 'icon_on_04.jpg' },
-  { name: '곡물/견과', off: 'icon_off_05.jpg', on: 'icon_on_05.jpg' },
+  { name: '채소', off: 'icon_off_01.jpg', on: 'icon_on_01.jpg', link: '/' },
+  { name: '과일', off: 'icon_off_02.jpg', on: 'icon_on_02.jpg', link: '/' },
+  { name: '나물/버섯', off: 'icon_off_03.jpg', on: 'icon_on_03.jpg', link: '/' },
+  { name: '꿀/뿌리', off: 'icon_off_04.jpg', on: 'icon_on_04.jpg', link: '/' },
+  { name: '곡물/견과', off: 'icon_off_05.jpg', on: 'icon_on_05.jpg', link: '/' },
 ];
 
 function MainRecProduct() {
@@ -15,8 +15,9 @@ function MainRecProduct() {
   return (
     <div className="main-rec-product">
       {categories.map((category, index) => (
-        <div
+        <a
           key={index}
+          href={category.link}
           className="category-item"
           onMouseEnter={() => setHovered(index)}
           onMouseLeave={() => setHovered(null)}
@@ -26,7 +27,8 @@ function MainRecProduct() {
             alt={category.name}
             className="category-icon"
           />
-        </div>
+          <p>{category.name}</p>
+        </a>
       ))}
     </div>
   );
