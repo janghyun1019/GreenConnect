510139ac855f8b27339c31ab7d75a9dee576a4bf
Jang Hyun <moon.p.a.hyun@gmail.com>
2025-03-11 10:25:40 +0900

    mainAPIGraph 위치 구현

--- src/greenconnect/src/main/Mainbody.js
+++ src/greenconnect/src/main/Mainbody.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import MainSlide from './MainSlide/MainSlide';
 import MainCmenu from './mainCmenu/mainCmenu'; // 대문자로 변경
+import MainAPIGraph from './mainAPIGraph/mainAPIGraph';
+
 
 function Mainbody() {
   return (
     <main style={{ flex: 1 }}>
       <MainSlide />
       <MainCmenu /> {/* 대문자로 변경 */}
+      <MainAPIGraph />
     </main>
   );
 }
