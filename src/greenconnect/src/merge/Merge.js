8b6734638269bbecefb2a3bf7a687bfb5f10bd6e
PCJ-93 <pw2900@naver.com>
2025-03-14 10:46:26 +0900

    mypage 깃허브 합치고 mapper수정함

--- src/greenconnect/src/merge/Merge.js
+++ src/greenconnect/src/merge/Merge.js
@@ -16,12 +16,51 @@ import Trade from '../admin/Trade';
 import Quality from '../admin/Quality';
 import System from '../admin/System';
 import CuSupport from '../admin/CuSupport';
+
+import PostDetail from "../postDetail/PostDetail";
+import PostDetailIntro from "../postDetail/PostDetailIntro";
+import MainPage from "../mainpageTemp1111/MainPage";
+import WritePost from "../postDetail/WritePost";
+import PostList from "../postList/PostList.js";
+import PostThumbnail from "../postList/PostThumbnail.js";
+import FindId from "../Page/Signup/FindId.js";
+import FindPassword from "../Page/Signup/FindPassword.js";
+import ResetPassword from "../Page/Signup/ResetPassword.js";
+import ModifyPostDetail from "../postDetail/ModifyPostDetail.js";
+import PostDetailReview from "../postDetail/PostDetailReview.js";
+import PayPage from "../pay/PayPage.js";
+import GpayChargePage from "../pay/GpayChargePage.js";
+
 function Merge() {
 
     return (
-        
-            <div className="merge-container">
+
+        <div className="merge-container">
             <Routes>
+
+
+                <Route path="/" element={<MainPage />} />
+                <Route path="/main" element={<MainPage />} />
+
+                <Route path="/writePost" element={<WritePost />} />
+                <Route path="/postDetail/:postId" element={<PostDetail />} />
+                <Route path="/postDetailReview/:postId" element={<PostDetailReview />} />
+                <Route path="/modifyPostDetail/:postId" element={<ModifyPostDetail />} />
+                <Route path="/postDetailIntro" element={<PostDetailIntro />} />
+                <Route path="/postList" element={<PostList />} />
+                <Route path="/postThumbnail" element={<PostThumbnail />} />
+
+                <Route path="/payPage/:postId" element={<PayPage />} />
+                {/* <Route path="/gpayChargePage" element={<GpayChargePage />} /> */}
+
+
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/findId" element={<FindId />} />
+                <Route path="/findPassword" element={<FindPassword />} />
+                <Route path="/resetPassword" element={<ResetPassword />} />
+
+
                 <Route path="/Mypage" element={<MyPage />} />
                 <Route path="/Cart" element={<Cart />} />
                 <Route path="/Address" element={<Address />} />
@@ -29,19 +68,17 @@ function Merge() {
                 <Route path="/likes" element={<Likes />} />
                 <Route path="/Chat" element={<Chat />} />
                 <Route path="/Profile" element={<Profile />} />
-                <Route path="/userinfo" element={<UserInfo />}/>
+                <Route path="/userinfo" element={<UserInfo />} />
                 <Route path="/Admin" element={<Admin />} />
                 <Route path="/Customer" element={<Customer />} />
                 {/*<Route path="/Trade" element={<Trade/>} />
                 <Route path="/Quality" element={<Quality/>} />
                 <Route path="/System" element={<System/>} />
                 <Route path="/CuSupport" element={<CuSupport/>} /> */}
-                
-                <Route path="/login" element={<Login/>}/>
-                <Route path="/signup" element={<Signup/>}/>
+
             </Routes>
-            </div>
-        
+        </div>
+
     );
 }
 
