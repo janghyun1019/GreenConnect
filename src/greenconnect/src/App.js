ef1ee0815628ddb438ab94eb126f860284e84c1b
Jang Hyun <moon.p.a.hyun@gmail.com>
2025-03-15 12:14:07 +0900

    merge 후 App.js 수정

--- src/greenconnect/src/App.js
+++ src/greenconnect/src/App.js
@@ -1,50 +1,13 @@
-import React from "react";
-import { BrowserRouter, Route, Routes, useInRouterContext } from "react-router-dom";
-import Signup from "./Page/Signup/Signup"; // 경로 조정
-import Login from "./Page/Signup/Login"; // 경로 조정
-import FindId from "./Page/Signup/FindId"; // 경로 조정
-import FindPassword from "./Page/Signup/FindPassword"; // 경로 조정
-import ResetPassword from "./Page/Signup/ResetPassword"; // 경로 조정
-import Header from "./common/Header";
-import Footer from "./common/Footer";
-import Main from "./main/main";
-import MarketInfoPage from "./Page/MarketInfo/MarketInfoPage";
-import NoticeBoard from "./Page/CustomerService/notice/noticeBoard";
-import NoticeDetail from "./Page/CustomerService/notice/NoticeDetail";
-import NoticeForm from "./Page/CustomerService/notice/NoticeForm";
-import Merge from "./merge/Merge"; // Merge 컴포넌트 추가
+import React from 'react';
+import Merge from './merge/Merge';    
+
+
 
 function App() {
-  const inRouter = useInRouterContext();
-  const RouterWrapper = inRouter ? React.Fragment : BrowserRouter;
-
   return (
-    <RouterWrapper basename={!inRouter ? process.env.PUBLIC_URL : undefined}>
-      <div className="app-container">
-        <Header />
-        <Routes>
-          {/* 메인 및 인증 관련 페이지 */}
-          <Route path="/" element={<Main />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/find-id" element={<FindId />} />
-          <Route path="/find-password" element={<FindPassword />} />
-          <Route path="/reset-password" element={<ResetPassword />} />
-
-          {/* 추가된 페이지 */}
-          <Route path="/market-info" element={<MarketInfoPage />} />
-          <Route path="/notice" element={<NoticeBoard />} />
-          <Route path="/notice/:id" element={<NoticeDetail />} />
-          <Route path="/notice/new" element={<NoticeForm />} />
-          <Route path="/notice/edit/:id" element={<NoticeForm />} />
-
-          {/* Merge 컴포넌트 추가 */}
-          <Route path="/merge" element={<Merge />} />
-        </Routes>
-        <Footer />
-      </div>
-    </RouterWrapper>
+    
+    <Merge/>
+    
   );
 }
-
 export default App;
