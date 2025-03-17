3eaf35341531ab6fe54079fdbf4e3dffb458e876
janghyun1019 <moon.p.a.hyun@gmail.com>
2025-03-17 21:06:29 +0900

    delete

--- src/greenconnect/src/components/input.js
+++
@@ -1,9 +0,0 @@
-export function Input({ value, onChange, placeholder }) {
-    return <input 
-        type="text" 
-        value={value} 
-        onChange={onChange} 
-        placeholder={placeholder} 
-        className="border rounded p-2 w-full"
-    />;
-}
