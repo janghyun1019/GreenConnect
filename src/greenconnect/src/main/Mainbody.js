fc089dd8f54ecefe5a3c7d65a932b6b2b03cfd10
janghyun1019 <moon.p.a.hyun@gmail.com>
2025-03-18 00:19:36 +0900

    MainContent restoration

--- src/greenconnect/src/main/Mainbody.js
+++ src/greenconnect/src/main/Mainbody.js
@@ -1,16 +1,20 @@
 import React from 'react';
-//import MainSlide from './MainSlide/MainSlide';
-//import MainCmenu from './mainCmenu/mainCmenu'; // 대문자로 변경
-//import MainAPIGraph from './mainAPIGraph/mainAPIGraph';
-//import MainSellerApply from './mainSellerApply/mainSellerApply';
-//import MinRecProduct from './mainRecProduct/mainRecProduct';
-import Main from '';
+import MainSlide from './MainSlide/MainSlide';
+import MainCmenu from './mainCmenu/mainCmenu'; // 대문자로 변경
+import MainAPIGraph from './mainAPIGraph/mainAPIGraph';
+import MainSellerApply from './mainSellerApply/mainSellerApply';
+import MinRecProduct from './mainRecProduct/mainRecProduct';
 
 
 function Mainbody() {
   return (
-	<div>잠깐 아무것도 없이!</div>
-
+    <main style={{ flex: 1 }}>
+      <MainSlide />
+      <MainCmenu /> {/* 대문자로 변경 */}
+      <MainAPIGraph />
+      <MainSellerApply />
+      <MinRecProduct />
+    </main>
   );
 }
 
--- src/greenconnect/src/main/main.js
+++ src/greenconnect/src/main/main.js
@@ -1,11 +1,11 @@
 import '../App.css';
 import React from "react";
 
-//import Mainbody from '../main/Mainbody';
+import Mainbody from '../main/Mainbody';
 
 function HomePage() {
   return (
-    <div>잠깐 아무것도 없이!</div>
+    <Mainbody />
   );
 }
 
