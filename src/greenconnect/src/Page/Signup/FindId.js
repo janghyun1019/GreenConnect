6f4b01ac305a74af4b3c6edf7935be09c8a02fc5
janghyun1019 <moon.p.a.hyun@gmail.com>
2025-03-21 06:57:15 +0900

    FindId

--- src/greenconnect/src/Page/Signup/FindId.js
+++ src/greenconnect/src/Page/Signup/FindId.js
@@ -1,25 +1,38 @@
 import axios from "axios";
 import { useState } from "react";
 
-function FindId() {
-    const [email, setEmail] = useState("");
-    const [foundId, setFoundId] = useState(null);
+import "../../common/commonPage.css";
+import "./Signup.css";
 
-    return (
-        <div>
-            <h1>아이디 찾기</h1>
-            이메일 <input type="email" onChange={(e) => setEmail(e.target.value)} /> <br />
-            <button onClick={()=>{
-                axios.post("/user/find-id", { email })
-                .then(response => {
-                    alert("이메일로 아이디를 전송했습니다.");
-                })
-                .catch(error => {
-                    alert("아이디 찾기 실패: " + error.response.data);
-                })
-            }}>아이디 찾기</button>
-        </div>
-    );
+
+function FindId() {
+	const [email, setEmail] = useState("");
+	const [foundId, setFoundId] = useState(null);
+
+	return (
+		<div className="signupContainer01">
+			<div className="title">
+				<i class="fa-solid fa-id-card"></i> 아이디 찾기
+			</div>
+			<div className="titlesub">아이디를 찾아드립니다.</div>
+			<div className="signupContainer">
+				<input
+					type="email"
+					placeholder="이메일을 입력하세요"
+					value={email}
+					onChange={(e) => setEmail(e.target.value)} /> <br /><br />
+				<button onClick={() => {
+					axios.post("/user/find-id", { email })
+						.then(response => {
+							alert("이메일로 아이디를 전송했습니다.");
+						})
+						.catch(error => {
+							alert("아이디 찾기 실패: " + error.response.data);
+						})
+				}}>아이디 찾기</button>
+			</div>
+		</div>
+	);
 }
 
 export default FindId;
