1296a86e4f085365060997d9cb9c82e05e99ae06
janghyun1019 <moon.p.a.hyun@gmail.com>
2025-03-21 06:51:29 +0900

    FindPassword css 작업

--- src/greenconnect/src/Page/Signup/FindPassword.js
+++ src/greenconnect/src/Page/Signup/FindPassword.js
@@ -1,24 +1,40 @@
 import axios from "axios";
 import { useState } from "react";
 
-function FindPassword() {
-    const [email, setEmail] = useState("");
+import "../../common/commonPage.css";
+import "./Signup.css";
 
-    return (
-        <div>
-            <h1>비밀번호 찾기</h1>
-            이메일 <input type="email" onChange={(e) => setEmail(e.target.value)} /> <br />
-            <button onClick={() => {
-                axios.post("/user/find-password", { email })
-                    .then(response => {
-                        alert("비밀번호 재설정 링크가 이메일로 전송되었습니다.");
-                    })
-                    .catch(error => {
-                        alert("비밀번호 찾기 실패: " + error.response.data);
-                    })
-            }}>비밀번호 찾기</button>
-        </div>
-    );
+function FindPassword() {
+	const [email, setEmail] = useState("");
+
+	return (
+		<div className="signupContainer01">
+			<div className="title">
+				<i class="fa-solid fa-lock-open"></i> 비밀번호 찾기
+			</div>
+			<div className="titlesub">새 비밀번호로 변경합니다.</div>
+			<div className="signupContainer">
+				<div className="suline">
+					<input
+						type="email"
+						placeholder="이메일을 입력하세요"
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+					/>
+				</div>
+				<br />
+				<button onClick={() => {
+					axios.post("/user/find-password", { email })
+						.then(response => {
+							alert("비밀번호 재설정 링크가 이메일로 전송되었습니다.");
+						})
+						.catch(error => {
+							alert("비밀번호 찾기 실패: " + error.response.data);
+						})
+				}}>비밀번호 찾기</button>
+			</div>
+		</div>
+	);
 }
 
-export default FindPassword;
+export default FindPassword; 
