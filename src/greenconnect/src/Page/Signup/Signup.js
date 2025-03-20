79b80ce993bd9d490ead224b595a4decf64aff80
janghyun1019 <moon.p.a.hyun@gmail.com>
2025-03-21 05:54:17 +0900

    Signup css 작업

--- src/greenconnect/src/Page/Signup/Signup.js
+++ src/greenconnect/src/Page/Signup/Signup.js
@@ -1,67 +1,133 @@
 import axios from "axios";
-import {useState} from 'react';
+import { useState } from 'react';
 
-function Signup(){
+import "./Signup.css";
 
-    let [userId, setUserId] = useState('');
-    let [password, setPassWord] = useState('');
-    let [confirmPw, setConfirmPw] = useState(""); 
-    let [userName, setUserName] = useState('');
-    let [nickName, setNickName] = useState('');
-    let [jumin, setJumin] = useState('');
-    let [tel, setTel] = useState('');
-    let [email, setEmail] = useState('');
+function Signup() {
+
+	let [userId, setUserId] = useState('');
+	let [password, setPassWord] = useState('');
+	let [confirmPw, setConfirmPw] = useState("");
+	let [userName, setUserName] = useState('');
+	let [nickName, setNickName] = useState('');
+	let [jumin, setJumin] = useState('');
+	let [tel, setTel] = useState('');
+	let [email, setEmail] = useState('');
 
 
 
-    return (
-        <div>
-            <div>
-            <h1>회원가입 페이지</h1>
 
-            아이디 <input type="text" onChange={(e) => setUserId(e.target.value)} /> <br />
-            비밀번호 <input type="password" onChange={(e) => setPassWord(e.target.value)} /> <br />
-            비밀번호 확인 <input type="password" onChange={(e) => setConfirmPw(e.target.value)} /> <br />
-            이름 <input type="text" onChange={(e) => setUserName(e.target.value)} /> <br />
-            닉네임 <input type="text" onChange={(e) => setNickName(e.target.value)} /> <br />
-            주민번호 <input type="text" onChange={(e) => setJumin(e.target.value)} /> <br />
-            휴대폰번호 <input type="tel" onChange={(e) => setTel(e.target.value)} /> <br />
-            이메일 <input type="email" onChange={(e) => setEmail(e.target.value)} /> <br />
-        </div>
-        
-        <button onClick={()=>{
-            axios.post(
-                "/user/signup",
-                {
-                    userId ,
-                    password ,
-                    userName ,
-                    nickName ,
-                    jumin ,
-                    tel ,
-                    email 
-                },
-                {
-                    headers :{
-                        'Content-type':'application/json'
-                    }
-                }
-            )
-            .then(response =>{
-                console.log(response.data);
-                if(response.data == 'ok'){
+	return (
+		<div className="signupContainer">
+			<div>
+				<h1>회원가입 페이지</h1>
 
-                }
-            })
-            .catch(error => {
-                console.log(error);
-            })
-            
-        }}>가입하기</button>
+				<div className="suline">
+					<input
+						type="text"
+						placeholder="아이디"
+						value={userId}
+						onChange={(e) => setUserId(e.target.value)}
+					/>
+				</div>
 
-        </div> //맨 앞 감싸는 div 끝
-        
-    );
+				<div className="suline">
+					<input
+						type="password"
+						placeholder="비밀번호"
+						value={password}
+						onChange={(e) => setPassWord(e.target.value)}
+					/>
+				</div>
+
+				<div className="suline">
+					<input
+						type="password"
+						placeholder="비밀번호 확인"
+						value={confirmPw}
+						onChange={(e) => setConfirmPw(e.target.value)}
+					/>
+				</div>
+
+				<div className="suline">
+					<input
+						type="text"
+						placeholder="이름"
+						value={userName}
+						onChange={(e) => setUserName(e.target.value)}
+					/>
+				</div>
+
+				<div className="suline">
+					<input
+						type="text"
+						placeholder="닉네임"
+						value={nickName}
+						onChange={(e) => setNickName(e.target.value)}
+					/>
+				</div>
+
+				<div className="suline">
+					<input
+						type="text"
+						placeholder="주민등록번호"
+						value={jumin}
+						onChange={(e) => setJumin(e.target.value)}
+					/>
+				</div>
+
+				<div className="suline">
+					<input
+						type="tel"
+						placeholder="휴대폰번호"
+						value={tel}
+						onChange={(e) => setTel(e.target.value)}
+					/>
+				</div>
+
+				<div className="suline">
+					<input
+						type="email"
+						placeholder="이메일"
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+					/>
+				</div>
+			</div>
+
+			<button onClick={() => {
+				axios.post(
+					"/user/signup",
+					{
+						userId,
+						password,
+						userName,
+						nickName,
+						jumin,
+						tel,
+						email
+					},
+					{
+						headers: {
+							'Content-type': 'application/json'
+						}
+					}
+				)
+					.then(response => {
+						console.log(response.data);
+						if (response.data == 'ok') {
+
+						}
+					})
+					.catch(error => {
+						console.log(error);
+					})
+
+			}}>가입하기</button>
+
+		</div> //맨 앞 감싸는 div 끝
+
+	);
 }
 
 export default Signup;
